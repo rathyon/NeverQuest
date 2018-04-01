47fdf82a0f12af5d88c909a575eccb431354f41b
Frederico Santos <fred.sant@hotmail.com>
2018-04-01 19:29:51 +0100

    Flip-deactivated

--- NeverQuest/Assets/Scripts/PlayerController.cs
+++ NeverQuest/Assets/Scripts/PlayerController.cs
@@ -6,11 +6,15 @@ public class PlayerController : MonoBehaviour {
 
     public float speed;
 
+	private bool facingright = true;
     //private Rigidbody2D rb2d;
+
+	//SpriteRenderer _spriteRenderer;
 
 	// Use this for initialization
 	void Start () {
         //rb2d = GetComponent<Rigidbody2D>();
+		//_spriteRenderer = GetComponent<SpriteRenderer> ();
 	}
 	
 	// Update is called once per frame
@@ -18,6 +22,16 @@ public class PlayerController : MonoBehaviour {
         float moveHorizontal = Input.GetAxis("Horizontal");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+		/*if (_spriteRenderer != null) {
+			if (moveHorizontal < 0 && facingright) {
+				facingright = !facingright;
+				_spriteRenderer.flipX = !_spriteRenderer.flipX;
+			}
+			if (moveHorizontal > 0 && !facingright) {
+				facingright = !facingright;
+				_spriteRenderer.flipX = !_spriteRenderer.flipX;
+			}
+		}*/
 
         transform.position += movement * speed * 0.1f;
 	}
