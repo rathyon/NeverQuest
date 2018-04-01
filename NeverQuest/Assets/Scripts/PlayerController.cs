3db5126a117ad98125ea81cad61f33ad01766e09
rathyon <sapphiron7@gmail.com>
2018-04-01 16:36:07 +0100

    Clean first prototype

---
+++ NeverQuest/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    //private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+        //rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+
+        transform.position += movement * speed * 0.1f;
+	}
+}
---
+++ NeverQuest/Assets/Scripts/TestMobController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TestMobController : MonoBehaviour {
+
+    public int HP;
+    public Text statsInfo;
+
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        statsInfo.text = "Enemy hp: " + HP.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(-0.025f, 0.0f);
+		if(HP <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            var trap = collision.GetComponent<TestTrapController>();
+
+            HP -= trap.damage;
+            statsInfo.text = "Enemy hp: " + HP.ToString();
+        }
+    }
+
+}
