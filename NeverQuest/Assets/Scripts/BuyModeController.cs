fe65a15df3785639f6a8c2a2fe8d3cec196a6334
Frederico Santos <fred.sant@hotmail.com>
2018-04-02 17:32:48 +0100

    Trap detection corrected

--- NeverQuest/Assets/Scripts/BuyModeController.cs
+++ NeverQuest/Assets/Scripts/BuyModeController.cs
@@ -68,7 +68,7 @@ public class BuyModeController : MonoBehaviour {
 		}
 	}
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("TrapOnce"))
         {
