56bd2bf2fbb369bfa28728c085a5bfda04cdeb6a
rathyon <sapphiron7@gmail.com>
2018-05-01 19:36:44 +0100

    Added proximity indicator and minimap indicators

---
+++ NeverQuest/Assets/Scripts/ProximityIndicatorController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProximityIndicatorController : MonoBehaviour
+{
+
+    public GameObject indicatorPrefab;
+    public float radius;
+
+    private List<GameObject> indicators = new List<GameObject>();
+    private List<GameObject> enemies = new List<GameObject>();
+
+    // THE RADIUS OF THE INDICATOR IS 3.8, USE THIS VALUE!
+    //private float radius = 3.0f;
+
+    private void AddIndicator(GameObject enemy)
+    {
+        Vector3 diff = enemy.transform.position - transform.position;
+        diff.Normalize();
+        Vector3 pos = (radius * diff) + transform.position;
+        var new_indicator = Instantiate(indicatorPrefab, pos, Quaternion.identity);
+        new_indicator.transform.parent = gameObject.transform;
+        indicators.Add(new_indicator);
+    }
+
+    private void UpdateIndicator(GameObject enemy, GameObject indicator)
+    {
+        Vector3 diff = enemy.transform.position - transform.position;
+        diff.Normalize();
+        indicator.transform.position = (radius * diff) + transform.position;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject[] enemiesArray = GameObject.FindGameObjectsWithTag("Mob");
+        foreach (GameObject enemy in enemiesArray)
+        {
+            enemies.Add(enemy);
+            AddIndicator(enemy);
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        GameObject[] enemiesArray = GameObject.FindGameObjectsWithTag("Mob");
+
+        // Add gameobject if its not in the list
+        for (int i = 0; i < enemiesArray.Length; i++)
+        {
+            bool contains = false;
+            for (int j = 0; j < enemies.Count; j++)
+            {
+                if (GameObject.ReferenceEquals(enemiesArray[i], enemies[j]))
+                {
+                    contains = true;
+                }
+
+                if (!contains)
+                {
+                    enemies.Add(enemiesArray[i]);
+                    AddIndicator(enemiesArray[i]);
+                }
+            }
+        }
+
+        //Update Indicators
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            if (enemies[i] != null)
+                UpdateIndicator(enemies[i], indicators[i]);
+            else
+                Destroy(indicators[i]);
+        }
+
+    }
+
+    public void removeEnemy(GameObject enemy)
+    {
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            if (GameObject.ReferenceEquals(enemies[i], enemy))
+            {
+                Destroy(indicators[i]);
+                indicators.RemoveAt(i);
+                enemies.RemoveAt(i);
+            }
+        }
+    }
+}
+
+
---
+++ NeverQuest/Assets/Scripts/TrapPlacement.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapPlacement : MonoBehaviour {
+
+	public GameObject canTRAP;
+	public GameObject cannotTRAP;
+	public GameObject player;
+
+	Trap trap;
+
+	GameObject can;
+	GameObject cannot;
+
+	public bool placement;
+
+	private bool invalidPlacement;
+	private PlayerController player_script; 
+
+	// Use this for initialization
+	void Start () {
+		placement = false;
+		can = Instantiate (canTRAP, transform.position, Quaternion.identity);
+		can.SetActive (false);
+		cannot = Instantiate (cannotTRAP, transform.position, Quaternion.identity);
+		cannot.SetActive (false);
+
+		invalidPlacement = false;
+		player_script = gameObject.GetComponentInParent<PlayerController>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (placement) {
+			Vector3 position;
+			if (player_script.facingRight) {
+				position = new Vector3 (player.transform.position.x + 2, player.transform.position.y + trap.placementY, transform.position.z);
+			} 
+			else {
+				position = new Vector3 (player.transform.position.x - 2, player.transform.position.y + trap.placementY, transform.position.z);
+			}
+			transform.position = position;
+			if (!invalidPlacement) {
+				cannot.SetActive (false);
+				can.transform.position = position;
+				can.SetActive (true);
+				if (Input.GetKeyDown (KeyCode.E)) {
+					
+					if (player_script.gold >= trap.cost) {
+							Instantiate (trap, position, Quaternion.identity);
+						player_script.gold -= trap.cost;
+						}
+				}
+			} else {
+				can.SetActive (false);
+				cannot.transform.position = position;
+				cannot.SetActive (true);	
+			}
+		} 
+		else {
+			cannot.SetActive (false);
+			can.SetActive (false);
+		}
+	}
+
+	public void setObject(Trap trapObject){
+		trap = trapObject ;
+		cannot.GetComponent<SpriteRenderer> ().sprite = can.GetComponent<SpriteRenderer> ().sprite = trap.GetComponent<SpriteRenderer> ().sprite;
+
+	}
+
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		if (collision.gameObject.CompareTag("Trap"))
+		{
+			invalidPlacement = true;
+
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.gameObject.CompareTag("Trap"))	{
+			invalidPlacement = false;
+		}
+	}
+}
