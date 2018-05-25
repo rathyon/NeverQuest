903e84abcc100e957e1d8cb4ecdb1ec09009da15
rathyon <sapphiron7@gmail.com>
2018-05-25 19:50:21 +0100

    Revamped (almost) all sprites

---
+++ NeverQuest/Assets/Scripts/UIDisplayController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIDisplayController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Canvas>().enabled = true;
+    }
+}
